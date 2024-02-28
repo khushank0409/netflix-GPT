c77eeb958ffdf8993bcaaf6383f0be48d4e3e839
khushank0409 <141670518+khushank0409@users.noreply.github.com>
2024-02-29 02:01:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Body from "./components/Body";
 function App() {
+<<<<<<< HEAD
+  return <Body/> 
+};
+=======
   return (
     <div className="text-3xl font-bold text-green-800">
           Namaste Everyone, let's build Netflix GPT
     </div>
   );
 }
+>>>>>>> d9e859fc076e7808e6eeab22dcf965db65eebbc8
 
 export default App;
